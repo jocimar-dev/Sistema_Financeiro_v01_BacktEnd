127b2f7130ac33fc89f39a211a90f8a8bafdf06b
Jocimar Neres <jocimar.silva@basis.com.br>
2020-04-02 15:20:40 -0300

    Remoção de recursos e inicio de relacão entre recursos reste

---
+++ src/main/java/com/example/algamoney/api/model/Lancamento.java
@@ -0,0 +1,11 @@
+package com.example.algamoney.api.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity]
+@Table(name = "lancamento")
+public class lancamento {
+
+    
+}
---
+++ src/main/java/com/example/algamoney/api/model/TipoLancamento.java
@@ -0,0 +1,4 @@
+package com.example.algamoney.api.model;
+
+public enum TipoLancamento {
+}
